8a8acfd05b0bbfea20348037a2a8f11870311c01
BasssamHillo <57863215+BasssamHillo@users.noreply.github.com>
2020-01-02 14:20:10 +0200

    report

--- Regression/pages/login.js
+++ Regression/pages/login.js
@@ -1,13 +1,14 @@
 const loginSelectors =require("../selectors/loginSelector");
+const { assert } = require('chai')
+const { expect } = require('chai')
 
 exports.fillUserDetails = (browser) =>{
     browser 
     .url('https://www.lancome-usa.com/register') // Go to a url
     .waitForElementVisible('body', 2000) // wait till page loads
-    .assert.title('Create account') // Make sure Site title matches
+    .assert.title('Create account1') // Make sure Site title matches
     .setValue(loginSelectors.email, 'email') // send values
     .setValue(loginSelectors.password, 'password') // send values
     .click(loginSelectors.signInBtn)
-    .pause(1000)
-    .end();
+    .pause(1000);
 }
--- Regression/tests/loginBase.js
+++ Regression/tests/loginBase.js
@@ -1,28 +1,28 @@
 const url = 'https://www.lancome-usa.com/';
-// const { assert } = require('chai')
-// const { expect } = require('chai')
+const loginPage = require("../pages/login");
+const logReport = require("mochawesome-screenshots/logReport");
+const { assert } = require('chai')
+const { expect } = require('chai')
 
-const loginPage =require("../pages/login");
+describe('Login', function () {
+
+  // before(function (browser, done) {
+  //   done();
+  // });
+
+  // after(function (browser, done) {
+  //   console.log("\n\n\n\n\n\ndone\n\n\n\n\n\n");
+  //   done();
+  // });
 
 
-module.exports = {
-	before : function(browser) {
-    console.log('Setting up...');
-  },
 
-  after : function(browser) {
-    console.log('Closing down...');
-  },
 
-  beforeEach : function(browser) {
-
-  },
-
-  afterEach : function(browser) {
-
-  },
-  'Fill user details' : function (browser) {
+  it('Fill user details', function (browser) {
+    logReport.log(this, "this is log");
     loginPage.fillUserDetails(browser);
-  }
+  });
+});
 
-};
+
+
