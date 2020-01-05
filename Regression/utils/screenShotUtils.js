bd30bf2a0c5cff6ebb865d142b9d66d3adfd0e80
Ibrahem Batta <18301319+ibatta@users.noreply.github.com>
2020-01-05 10:24:59 +0200

    clean copy

--- Regression/utils/screenShotUtils.js
+++ Regression/utils/screenShotUtils.js
@@ -3,9 +3,10 @@
  exports.takeScreenShot = (testOBJ, browser, Message) =>{
     const now = new Date()  ;
     const secondsSinceEpoch = Math.round(now.getTime() / 1000)  ;
-    var ScreenShotName = "SCS_"+secondsSinceEpoch+".png"; 
+    var ScreenShotName = "SCS_"+secondsSinceEpoch+Math.floor(Math.random(1,0)*1000)+".png"; 
     browser.saveScreenshot("./customReportDir/screenshots/"+ScreenShotName);
-    logReport.log(testOBJ, "Taking Screenshot:<br> <img src='screenshots/"+ScreenShotName+"'>");
+    logReport.log(testOBJ, "Taking Screenshot:<br>"+Message+"<br><img src='screenshots/"+ScreenShotName+"'>");
+    console.log("Taking Screenshot: "+ScreenShotName);
     logReport.setScreenshot(testOBJ, ScreenShotName, Message+"");
 
  }
