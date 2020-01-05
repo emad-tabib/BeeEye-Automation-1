9538b6003d7007bec577f4f86c42b748f0f0d134
BasssamHillo <57863215+BasssamHillo@users.noreply.github.com>
2020-01-05 16:03:12 +0200

    read config -- setup

--- Regression/Page/login.js
+++ Regression/Page/login.js
@@ -1,11 +1,13 @@
 const loginSelectors =require("../Selectors/loginSelectors");
+const setup=require("../utils/setup");
 const { assert } = require('chai').assert
 const { expect } = require('chai')
 
 exports.fillUserDetails = (browser) =>{
+
+  setup.lunchBrowser(browser,'/register');
+  
     browser 
-    .url('https://www.lancome-usa.com/register') // Go to a url
-    .waitForElementVisible('body', 2000) // wait till page loads
     .getTitle(function(title) {
         console.log("Page title is: "+title);
         this.assert.ok(title.includes("Create account"));
