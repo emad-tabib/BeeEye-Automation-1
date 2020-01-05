a88c6af05d7058f3480833c55fad89b18201c423
BasssamHillo <57863215+BasssamHillo@users.noreply.github.com>
2020-01-05 11:21:54 +0200

    after

--- Regression/tests/LancomeLoginBase.js
+++ Regression/tests/LancomeLoginBase.js
@@ -7,34 +7,49 @@ const { expect } = require('chai')
 
 describe('Login', function () {
 
-   before(function (browser, done) {
-    logReport.log(this, "Welcome to ITG default nightwatch template"); 
-    done();
+    before(function (browser, done) {
+        //logReport.log(this, "Welcome to ITG default nightwatch template"); 
+        console.log("\n\n\n\n\nWelcome to ITG default nightwatch template\n\n\n\n\n\n\n");
+        done();
 
-   });
+    });
 
-   after(function (browser, done) {
-    logReport.log(this, "finished case"); 
-     done();
-   });
-  
-it('Fill user details', function(browser) {
- 
-	logReport.log(this, "this is log");
-    loginPage.fillUserDetails(browser);
+    after(function (browser, done) {
+        //logReport.log(this, "finished case"); 
+        browser.end(function () {
+            console.log("\n\n\n\n\nEnd\n\n\n\n\n\n\n");
+            done();
+        });
+    });
+    afterEach(function (browser, done) {
+        browser.end(function () {
 
-    screenShotUtils.takeScreenShot(this,browser,"this is the results");
-    browser.end();
-});
+            done();
+        });
 
-it('Fill user details 2', function(browser) {
- 
-	logReport.log(this, "this is log");
-    loginPage.fillUserDetails(browser);
+    });
 
-    screenShotUtils.takeScreenShot(this,browser,"this is the results");
-    browser.end();
-});
+    beforeEach(function (browser, done) {
+        done();
+    });
+
+    it('Fill user details', function (browser) {
+
+        logReport.log(this, "this is log");
+        loginPage.fillUserDetails(browser);
+
+        screenShotUtils.takeScreenShot(this, browser, "this is the results");
+        browser.end();
+    });
+
+    it('Fill user details 2', function (browser) {
+
+        logReport.log(this, "this is log");
+        loginPage.fillUserDetails(browser);
+
+        screenShotUtils.takeScreenShot(this, browser, "this is the results");
+        browser.end();
+    });
 
 });
 
