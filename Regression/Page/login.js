5668c556b0505c3841d3e0be1281416b70e53693
BasssamHillo <57863215+BasssamHillo@users.noreply.github.com>
2020-01-06 08:35:49 +0200

    Selectors

--- Regression/Page/login.js
+++ Regression/Page/login.js
@@ -1,19 +1,19 @@
-const loginSelectors =require("../Selectors/loginSelectors");
-const setup=require("../utils/setup");
+const loginSelectors = require("../Selectors/loginSelectors");
+const setup = require("../utils/setup");
 const { assert } = require('chai').assert
 const { expect } = require('chai')
 
-exports.fillUserDetails = (browser) =>{
+exports.fillUserDetails = (browser) => {
 
-  setup.lunchBrowser(browser,'/register');
-  
-    browser 
-    .getTitle(function(title) {
-        console.log("Page title is: "+title);
-        this.assert.ok(title.includes("Create account"));
-      })
-    .setValue(loginSelectors.email, 'email') // send values
-    .setValue(loginSelectors.password, 'password') // send values
-    .click(loginSelectors.signInBtn)
-    .pause(1000); 
+  setup.lunchBrowser(browser, '/register');
+  var login = browser.page.loginSelectors();
+  login
+    .getTitle(function (title) {
+      console.log("Page title is: " + title);
+      this.assert.ok(title.includes("Create account"));
+    })
+    .setValue('@email', 'email') // send values
+    .setValue('@password', 'password') // send values
+    .click('@signInBtn')
+    .pause(1000);
 }
