047d88c1e2baa8e0cf6aa98efb69c0f3aafd807a
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-02-13 10:28:03 +0200

    User Status Bar_Add assertion to check if the element is present
    Add "assert.elementPresent" after "waitForElementVisible" statement

--- Regression/Page/UserstatusBar.js
+++ Regression/Page/UserstatusBar.js
@@ -1,82 +1,108 @@
 const UserStatusBarSelector = require("../Selectors/UserStatusBarSelector");
 const loginSelectors = require("../Selectors/loginSelectors");
 const configrationReader = require("../utils/configrationReader");
-const { assert } = require('chai').assert
-const { expect } = require('chai')
+const {
+  assert
+} = require('chai').assert
+const {
+  expect
+} = require('chai')
 
 //Check if the UserStatusBar is display or not
-exports.IsUserStatusBarDisplay = (browser) =>{
+exports.IsUserStatusBarDisplay = (browser) => {
   browser
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     .waitForElementVisible(UserStatusBarSelector.elements.UserLoggedInMenu)
-      .waitForElementVisible(loginSelectors.elements.UserMenu)
-      .waitForElementVisible(UserStatusBarSelector.elements.UserMenuDropdown)
-      //to check if itg(According to what is the username) is display or not
-      .assert.containsText(loginSelectors.elements.UserMenu, configrationReader.getUserName())
-      .click(UserStatusBarSelector.elements.UserMenuDropdown,function(result){
-          browser
+    .assert.elementPresent(UserStatusBarSelector.elements.UserLoggedInMenu)
+
+    .waitForElementVisible(loginSelectors.elements.UserMenu)
+    .assert.elementPresent(loginSelectors.elements.UserMenu)
+
+    .waitForElementVisible(UserStatusBarSelector.elements.UserMenuDropdown)
+    .assert.elementPresent(UserStatusBarSelector.elements.UserMenuDropdown)
+
+    //to check if itg(According to what is the username) is display or not
+    .assert.containsText(loginSelectors.elements.UserMenu, configrationReader.getUserName())
+    .click(UserStatusBarSelector.elements.UserMenuDropdown, function (result) {
+      browser
         .waitForElementVisible(UserStatusBarSelector.elements.About, configrationReader.getPeriod())
+        .assert.elementPresent(UserStatusBarSelector.elements.About)
+
         .waitForElementVisible(UserStatusBarSelector.elements.LogOut, configrationReader.getPeriod())
+        .assert.elementPresent(UserStatusBarSelector.elements.LogOut)
+
         .waitForElementVisible(UserStatusBarSelector.elements.SystemDiagnostics, configrationReader.getPeriod())
-      })
+        .assert.elementPresent(UserStatusBarSelector.elements.SystemDiagnostics)
+    })
 
 
-    .pause(configrationReader.getPauseValue()); 
+    .pause(configrationReader.getPauseValue());
 
 }
 
 //Check if "About" Link takes you to correct page
-exports.AboutLink = (browser) =>{
+exports.AboutLink = (browser) => {
   browser
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     .waitForElementVisible(UserStatusBarSelector.elements.UserMenuDropdown)
-      .click(UserStatusBarSelector.elements.UserMenuDropdown,function(result){
-          browser
+    .assert.elementPresent(UserStatusBarSelector.elements.UserMenuDropdown)
+
+    .click(UserStatusBarSelector.elements.UserMenuDropdown, function (result) {
+      browser
         .waitForElementVisible(UserStatusBarSelector.elements.About, configrationReader.getPeriod())
-        .click(UserStatusBarSelector.elements.About,function(result){
-            browser
+        .assert.elementPresent(UserStatusBarSelector.elements.About)
+        .click(UserStatusBarSelector.elements.About, function (result) {
+          browser
             .waitForElementVisible(UserStatusBarSelector.elements.AboutPage, configrationReader.getPeriod())
+            .assert.elementPresent(UserStatusBarSelector.elements.AboutPage)
         })
-      })
+    })
 
 
-    .pause(configrationReader.getPauseValue()); 
+    .pause(configrationReader.getPauseValue());
 
 }
 
 //Check if "system diagnostics" Link takes you to correct page
-exports.SystemDiagnosticsLink = (browser) =>{
+exports.SystemDiagnosticsLink = (browser) => {
   browser
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     .waitForElementVisible(UserStatusBarSelector.elements.UserMenuDropdown)
-      .click(UserStatusBarSelector.elements.UserMenuDropdown,function(result){
-          browser
+    .assert.elementPresent(UserStatusBarSelector.elements.UserMenuDropdown)
+
+    .click(UserStatusBarSelector.elements.UserMenuDropdown, function (result) {
+      browser
         .waitForElementVisible(UserStatusBarSelector.elements.SystemDiagnostics, configrationReader.getPeriod())
-        .click(UserStatusBarSelector.elements.SystemDiagnostics,function(result){
-            browser
+        .assert.elementPresent(UserStatusBarSelector.elements.SystemDiagnostics)
+        .click(UserStatusBarSelector.elements.SystemDiagnostics, function (result) {
+          browser
             .waitForElementVisible(UserStatusBarSelector.elements.SystemDiagnosticsPage, configrationReader.getPeriod())
+            .assert.elementPresent(UserStatusBarSelector.elements.SystemDiagnosticsPage)
         })
-      })
+    })
 
 
-    .pause(configrationReader.getPauseValue()); 
+    .pause(configrationReader.getPauseValue());
 
 }
 //Check if "Log Out" Link takes you to correct page
-exports.LogOutLink = (browser) =>{
+exports.LogOutLink = (browser) => {
   browser
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     .waitForElementVisible(UserStatusBarSelector.elements.UserMenuDropdown)
-      .click(UserStatusBarSelector.elements.UserMenuDropdown,function(result){
-          browser
+    .assert.elementPresent(UserStatusBarSelector.elements.UserMenuDropdown)
+    .click(UserStatusBarSelector.elements.UserMenuDropdown, function (result) {
+      browser
         .waitForElementVisible(UserStatusBarSelector.elements.LogOut, configrationReader.getPeriod())
-        .click(UserStatusBarSelector.elements.LogOut,function(result){
-            browser
+        .assert.elementPresent(UserStatusBarSelector.elements.LogOut)
+        .click(UserStatusBarSelector.elements.LogOut, function (result) {
+          browser
             .waitForElementVisible(UserStatusBarSelector.elements.SignInTitle, configrationReader.getPeriod())
+            .assert.elementPresent(UserStatusBarSelector.elements.SignInTitle)
         })
-      })
+    })
 
 
-    .pause(configrationReader.getPauseValue()); 
+    .pause(configrationReader.getPauseValue());
 
 }
