1a14292ba1dd388fa82632316a9512f7b7ceea2e
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-02-18 10:52:09 +0200

    Add msg for every WaitForElementVisible/ assert
    These error msg will appear just when the test is failed  and are display in console and in Report

--- Regression/Page/Dashboard.js
+++ Regression/Page/Dashboard.js
@@ -10,56 +10,53 @@ const {
 
 //Left
 exports.LeftSideInDashboardPage = (browser) => {
-    var Dashboard = browser.page.DashboardSelector();
-    Dashboard
+    browser
         .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
-        .waitForElementVisible(DashboardSelector.elements.left)
-        .assert.elementPresent(DashboardSelector.elements.left)
+        .waitForElementVisible(DashboardSelector.elements.left, configrationReader.getPeriod(),'Test is failed because the left section was not found in Dashboard Page')
+        .assert.elementPresent(DashboardSelector.elements.left,'Test is failed because the left section was not found in Dashboard Page')
 
-        .waitForElementVisible(DashboardSelector.elements.SmallCard)
-        .assert.elementPresent(DashboardSelector.elements.SmallCard)
+        .waitForElementVisible(DashboardSelector.elements.SmallCard,'Test is failed because the card which contains Best Flow(GINI) is not exists' )
+        .assert.elementPresent(DashboardSelector.elements.SmallCard,'Test is failed because the card which contains Best Flow(GINI) is not exists')
 
-        .assert.containsText(DashboardSelector.elements.SmallCard, 'Best Flow(GINI)')
+        .assert.containsText(DashboardSelector.elements.SmallCard, 'Best Flow(GINI)', 'Test is failed because the card does not contain Best Flow(GINI)')
 
-        .waitForElementVisible(DashboardSelector.elements.BigCard)
-        .assert.elementPresent(DashboardSelector.elements.BigCard)
+        .waitForElementVisible(DashboardSelector.elements.BigCard,'Test is failed because the card which contains Top Flows is not exists')
+        .assert.elementPresent(DashboardSelector.elements.BigCard,'Test is failed because the card which contains Top Flows is not exists')
 
-        .assert.containsText(DashboardSelector.elements.BigCard, 'Top Flows')
-        .pause(configrationReader.getPauseValue());
+        .pause(configrationReader.getPauseValue())
+        .assert.containsText(DashboardSelector.elements.BigCard, 'Top Flows' , 'Test is failed because the card does not contain Top Flows')
+     
 }
 
 
 //Center
 exports.CenterSideInDashboardPage = (browser) => {
-    var Dashboard = browser.page.DashboardSelector();
-    Dashboard
+    browser
         .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
-        .waitForElementVisible(DashboardSelector.elements.center)
-        .assert.elementPresent(DashboardSelector.elements.center)
+        .waitForElementVisible(DashboardSelector.elements.center,'Test is failed because the center section was not found in Dashboard Page')
+        .assert.elementPresent(DashboardSelector.elements.center,'Test is failed because the center section was not found in Dashboard Page')
 
-        .waitForElementVisible(DashboardSelector.elements.SmallCard)
-        .assert.elementPresent(DashboardSelector.elements.SmallCard)
+        .waitForElementVisible(DashboardSelector.elements.SmallCard,'Test is failed because the card which contains System Statistics is not exists')
+        .assert.elementPresent(DashboardSelector.elements.SmallCard,'Test is failed because the card which contains System Statistics is not exists')
 
-        .assert.containsText(DashboardSelector.elements.center, 'System Statistics')
+        .assert.containsText(DashboardSelector.elements.center, 'System Statistics','Test is failed because the card does not contain System Statistics')
 
-        .waitForElementVisible(DashboardSelector.elements.BigCard)
-        .assert.elementPresent(DashboardSelector.elements.BigCard)
+        .waitForElementVisible(DashboardSelector.elements.BigCard , 'Test is failed because the card which contains Platform Statistics is not exists')
+        .assert.elementPresent(DashboardSelector.elements.BigCard , 'Test is failed because the card which contains Platform Statistics is not exists')
 
-        .assert.containsText(DashboardSelector.elements.center, 'Platform Statistics')
-        .pause(configrationReader.getPauseValue());
+        .pause(configrationReader.getPauseValue())
+        .assert.containsText(DashboardSelector.elements.center, 'Platform Statistics', 'Test is failed because the card does not contain Platform Statistics');
 }
 
 //Right
 exports.RightSideInDashboardPage = (browser) => {
-    var Dashboard = browser.page.DashboardSelector();
-    Dashboard
+    browser
         .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
-        .waitForElementVisible(DashboardSelector.elements.right)
-        .assert.elementPresent(DashboardSelector.elements.right)
+        .waitForElementVisible(DashboardSelector.elements.right,'Test is failed because the right section was not found in Dashboard Page')
+        .assert.elementPresent(DashboardSelector.elements.right,'Test is failed because the right section was not found in Dashboard Page')
 
-        .waitForElementVisible(DashboardSelector.elements.RecentViewedFlows)
-        .assert.elementPresent(DashboardSelector.elements.RecentViewedFlows)
-
-        .assert.containsText(DashboardSelector.elements.RecentViewedFlows, 'Recent Viewed Flows')
-        .pause(configrationReader.getPauseValue());
+        .waitForElementVisible(DashboardSelector.elements.RecentViewedFlows, 'Test is failed because the card which contains RecentViewedFlows is not exists')
+        .assert.elementPresent(DashboardSelector.elements.RecentViewedFlows, 'Test is failed because the card which contains RecentViewedFlows is not exists')
+        .pause(configrationReader.getPauseValue())
+        .assert.containsText(DashboardSelector.elements.RecentViewedFlows, 'Recent Viewed Flows','Test is failed because the card does not contain Recent Viewed Flows' );
 }
