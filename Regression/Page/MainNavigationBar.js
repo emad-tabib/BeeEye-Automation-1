db76c40c8909627cd6981e7d17d32062f86aac8a
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-02-19 11:54:46 +0200

    Add comments in  MainNavigationBa
    Add comment for every line in the code

--- Regression/Page/MainNavigationBar.js
+++ Regression/Page/MainNavigationBar.js
@@ -1,3 +1,4 @@
+//This code to Test if Logo is display or not , and check every Link in this Navigation Bar if navigates you the correct screen
 const MainNavigationBarSelectors = require("../Selectors/MainNavigationBarSelectors");
 const DashboardSelector = require("../Selectors/DashboardSelector");
 const FlowsSelector = require("../Selectors/FlowsSelector");
@@ -13,74 +14,91 @@ const configrationReader = require("../utils/configrationReader");
 //Check Logo and Main Navigation Bar
 exports.MainNavigationBar = (browser) => {
   browser
-    .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
-    //Logo
+    //wait the body to be loadded
+    .waitForElementVisible('body', configrationReader.getPeriod()) // Wait till page loads
+    //check the Logo if it is display
     .waitForElementVisible(MainNavigationBarSelectors.elements.Logo, 'Test is failed because the logo was not found in the Main Navigation Bar')
     .assert.elementPresent(MainNavigationBarSelectors.elements.Logo, 'Test is failed because the logo was not found in the Main Navigation Bar')
 
-    //Navigation
+    //Check if the Main Navigation Bar is display or not
     .waitForElementVisible(MainNavigationBarSelectors.elements.NavigationBar, 'Test is failed because the Navigation Bar was not found in the site')
     .assert.elementPresent(MainNavigationBarSelectors.elements.NavigationBar, 'Test is failed because the Navigation Bar was not found in the site')
+    //get pause
     .pause(configrationReader.getPauseValue());
 }
 
-//DASHBOARD
+//Check the DASHBOARD Link in Main Navigation Bar if it takes you to the correct Page (Dashboard Page)
 exports.DashboardNavigation = (browser) => {
   browser
-    .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //wait the body to be loadded
+    .waitForElementVisible('body', configrationReader.getPeriod()) // Wait till page loads
+    //Check if Dashboard Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Dashboard, 'Test is failed because the Dashboard Link was not found in the Main Navigation Bar')
     .assert.elementPresent(MainNavigationBarSelectors.elements.Dashboard, 'Test is failed because the Dashboard Link was not found in the Main Navigation Bar')
-
+    //Check what happen after you click on Dashboard link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Dashboard, function (result) {
       browser
-      //this to make sure that you are in Dashboard Page when you click on Dashboard Link
+        //Here to make sure that you are in Dashboard Page when you click on Dashboard Link by check if Small Card is display or not
         .waitForElementVisible(DashboardSelector.elements.SmallCard, 'Test is failed After you click on Dashboard Link because the link does not take you to Dashboard Page')
         .assert.elementPresent(DashboardSelector.elements.SmallCard, 'Test is failed After you click on Dashboard Link because the link does not take you to Dashboard Page')
     })
+    //get pause
     .pause(configrationReader.getPauseValue());
 }
 
-//FLOWS
+//Check the FLOWS Link in Main Navigation Bar if it takes you to the correct Page (FLOWS Page)
 exports.FlowsNavigation = (browser) => {
   browser
+    //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if Flows Link is display in Main Navigation Bar
     .waitForElementVisible(FlowsSelector.elements.NavFlows, 'Test is failed because the Flows Link was not found in the Main Navigation Bar')
     .assert.elementPresent(FlowsSelector.elements.NavFlows, 'Test is failed because the Flows Link was not found in the Main Navigation Bar')
-
+    //Check what happen after you click on Flows link in the Navigation Bar
     .click(FlowsSelector.elements.NavFlows, function (result) {
       browser
+        //Here to make sure that you are in Flows Page when you click on Flows Link by check if Flow Title is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.FlowTitle, 'Test is failed After you click on Flows Link because the link does not take you to Flows Page')
         .assert.elementPresent(MainNavigationBarSelectors.elements.FlowTitle, 'Test is failed After you click on Flows Link because the link does not take you to Flows Page')
     })
+    //get pause
     .pause(configrationReader.getPauseValue());
 }
 
-//PRODUCTION
+//Check the PRODUCTION Link in Main Navigation Bar if it takes you to the correct Page (PRODUCTION Page)
 exports.ProductionNavigation = (browser) => {
   browser
+    //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if Production Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Production, 'Test is failed because the Production Link was not found in the Main Navigation Bar')
     .assert.elementPresent(MainNavigationBarSelectors.elements.Production, 'Test is failed because the Production Link was not found in the Main Navigation Bar')
-
+    //Check what happen after you click on Production link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Production, function (result) {
       browser
+        //Here to make sure that you are in Production Page when you click on Production Link by check if product count is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.ProductsCount, 'Test is failed After you click on Production Link because the link does not take you to Production Page')
         .assert.elementPresent(MainNavigationBarSelectors.elements.ProductsCount, 'Test is failed After you click on Production Link because the link does not take you to Production Page')
     })
+    //get pause
     .pause(configrationReader.getPauseValue());
 }
 
 //EXPLAIN
 exports.ExplainNavigation = (browser) => {
   browser
+    //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if Explain Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Explain, 'Test is failed because the Explain Link was not found in the Main Navigation Bar')
     .assert.elementPresent(MainNavigationBarSelectors.elements.Explain, 'Test is failed because the Explain Link was not found in the Main Navigation Bar')
-
+    //Check what happen after you click on Explain link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Explain, function (result) {
       browser
+        //Here to make sure that you are in Explain Page when you click on Explain Link by check if Explain Wrapper is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.ExplainWrapper, 'Test is failed After you click on Explain Link because the link does not take you to Explain Page')
         .assert.elementPresent(MainNavigationBarSelectors.elements.ExplainWrapper, 'Test is failed After you click on Explain Link because the link does not take you to Explain Page')
     })
+    //get pause
     .pause(configrationReader.getPauseValue());
 }
--- Regression/tests/MainNavigationBarTests/MainNavigationBarTests.js
+++ Regression/tests/MainNavigationBarTests/MainNavigationBarTests.js
@@ -13,75 +13,85 @@ const {
 
 describe('Main Navigation Bar : Test if Logo is display or not , and check every Link in this Navigation Bar if navigates you the correct screen  ', function () {
 
-   before(function (browser, done) {
-    logReport.log(this, "Test Cases for Main Navigation Bar");
-        console.log("Test Cases for Main Navigation Bar"); 
-    done();
+    before(function (browser, done) {
+        logReport.log(this, "Test Cases for Main Navigation Bar");
+        console.log("Test Cases for Main Navigation Bar");
+        done();
 
-   });
+    });
 
-   after(function (browser, done) {
-    browser.end(function () {
-        console.log("End Browser");
+    after(function (browser, done) {
+        browser.end(function () {
+            console.log("End Browser");
+            done();
+        });
+    });
+
+    beforeEach(function (browser, done) {
+        logReport.log(this, "Open the site");
+        setup.lunchBrowser(browser, '');
+        logReport.log(this, "Login with valid Information");
+        loginPage.LoginWithValidInformation(browser);
+        setup.lunchBrowser(browser, '/dashboard');
+        logReport.log(this, "Check The title of the Page");
+        title.getTitle(browser);
+        console.log("before each");
         done();
     });
-});
-  
-beforeEach(function (browser, done) {
-    logReport.log(this, "Open the site");
-    setup.lunchBrowser(browser,'');
-    logReport.log(this, "Login with valid Information");
-    loginPage.LoginWithValidInformation(browser);
-    setup.lunchBrowser(browser, '/dashboard');
-    logReport.log(this, "Check The title of the Page");
-    title.getTitle(browser);
-    console.log("before each");
-    done();
-});
 
-afterEach(function (browser, done) {
-    done();
-});
+    afterEach(function (browser, done) {
+        done();
+    });
 
-//Check Logo and Navigation Bar
-it('Main Navigation Bar', function(browser) {
-    setup.logTestDetails(this, "Check if Logo and Navigation Bar are display after the Login successfully")
-    MainNavigationBar.MainNavigationBar(browser);
-    screenShotUtils.takeScreenShot(this,browser,"Here is the screenshot for Logo and Navigation Bar if are exists");
-    browser.end();
-});
+    //Check Logo and Navigation Bar
+    it('Main Navigation Bar', function (browser) {
+        setup.logTestDetails(this, "Check if Logo and Navigation Bar are display after the Login successfully")
+        //Call MainNavigationBar function to check the Logo and check the Main Navigation Bar
+        MainNavigationBar.MainNavigationBar(browser);
+        //Take screenshot for the result
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for Logo and Navigation Bar if are exists");
+        browser.end();
+    });
 
-//Dashboard
-it('DASHBOARD', function(browser) {
-    setup.logTestDetails(this, "Try to Click on DASHBOARD Link and check if it takes you to correct screen")
-    MainNavigationBar.DashboardNavigation(browser);
-    screenShotUtils.takeScreenShot(this,browser,"Here is the screenshot after you click on DASHBOARD Link");
-    browser.end();
-});
+    //Check the Dashboard Link
+    it('DASHBOARD', function (browser) {
+        setup.logTestDetails(this, "Try to Click on DASHBOARD Link and check if it takes you to correct screen")
+        //Call DashboardNavigation function to check the Dashboard link and if it navigates you to the correct page
+        MainNavigationBar.DashboardNavigation(browser);
+        //Take screenshot for the result
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you click on DASHBOARD Link");
+        browser.end();
+    });
 
-//Flows
-it('FLOWS', function(browser) {
-    setup.logTestDetails(this, "Try to Click on FLOWS Link and check if it takes you to correct screen")
-    MainNavigationBar.FlowsNavigation(browser);
-    screenShotUtils.takeScreenShot(this,browser,"Here is the screenshot after you click on FLOWS Link");
-    browser.end();
-});
+    //Check the Flows Link
+    it('FLOWS', function (browser) {
+        setup.logTestDetails(this, "Try to Click on FLOWS Link and check if it takes you to correct screen")
+        //Call FlowsNavigation function to check the Flows link and if it navigates you to the correct page
+        MainNavigationBar.FlowsNavigation(browser);
+        //Take screenshot for the result
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you click on FLOWS Link");
+        browser.end();
+    });
 
 
-//Production
-it('PRODUCTION', function(browser) {
-    setup.logTestDetails(this, "Try to Click on PRODUCTION Link and check if it takes you to correct screen")
-    MainNavigationBar.ProductionNavigation(browser);
-    screenShotUtils.takeScreenShot(this,browser,"Here is the screenshot after you click on PRODUCTION Link");
-    browser.end();
-});
+    //Check the Production Link
+    it('PRODUCTION', function (browser) {
+        setup.logTestDetails(this, "Try to Click on PRODUCTION Link and check if it takes you to correct screen")
+        //Call ProductionNavigation function to check the Production link and if it navigates you to the correct page
+        MainNavigationBar.ProductionNavigation(browser);
+        //Take screenshot for the result
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you click on PRODUCTION Link");
+        browser.end();
+    });
 
 
-//Explain
-it('EXPLAIN', function(browser) {
-    setup.logTestDetails(this, "Try to Click on EXPLAIN Link and check if it takes you to correct screen")
-    MainNavigationBar.ExplainNavigation(browser);
-    screenShotUtils.takeScreenShot(this,browser,"Here is the screenshot after you click on EXPLAIN Link");
-    browser.end();
-});
+    //Check the Explain Link
+    it('EXPLAIN', function (browser) {
+        setup.logTestDetails(this, "Try to Click on EXPLAIN Link and check if it takes you to correct screen")
+        //Call ExplainNavigation function to check the Explain link and if it navigates you to the correct page
+        MainNavigationBar.ExplainNavigation(browser);
+        //Take screenshot for the result
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you click on EXPLAIN Link");
+        browser.end();
+    });
 });
