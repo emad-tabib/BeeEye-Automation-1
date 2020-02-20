5b63deca29bfe182346f5d45bff69fcd2300ff84
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-02-20 09:18:30 +0200

    Add comments in Login Page

--- Regression/Page/login.js
+++ Regression/Page/login.js
@@ -12,13 +12,20 @@ const setup = require("../utils/setup");
 //Login without password
 exports.LoginWithoutPassword = (browser) => {
   browser
+  //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if email field is display 
     .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
+    //Check if Password field is display 
     .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    //set the value for password field
     .setValue(loginSelectors.elements.password, '') // send values
+    //Check if Sign in button is display or not
     .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
+    //Here to check that the login was not successfully and the user is still in Log in Page, also error msg is display
     .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was without password')
     .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was without password')
     .pause(configrationReader.getPauseValue());
@@ -28,13 +35,20 @@ exports.LoginWithoutPassword = (browser) => {
 //Login Without username
 exports.LoginWithoutUsername = (browser) => {
   browser
+  //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if email field is display 
     .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    //set the value for the email field
     .setValue(loginSelectors.elements.email, '') // send values
+    //Check if Password field is display
     .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
+    //Check if Sign in button is display or not
     .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
+    //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
     .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was Without username')
     .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was without username')
     .pause(configrationReader.getPauseValue());
@@ -44,13 +58,20 @@ exports.LoginWithoutUsername = (browser) => {
 //Login With wrong password
 exports.LoginWithWrongPassword = (browser) => {
   browser
+  //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if email field is display
     .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
+    //Check if Password field is display
     .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    //set the value for password field
     .setValue(loginSelectors.elements.password, 'jj') // send values
+    //Check if Sign in button is display or not
     .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
+    //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
     .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was With wrong password')
     .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was  With wrong password')
     .pause(configrationReader.getPauseValue());
@@ -59,13 +80,20 @@ exports.LoginWithWrongPassword = (browser) => {
 //Login with wrong username
 exports.LoginWithWrongUsername = (browser) => {
   browser
+  //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+    //Check if email field is display
     .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    //set the value for the email field
     .setValue(loginSelectors.elements.email, 'user') // send values
+    //Check if Password field is display
     .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
+    //Check if Sign in button is display or not
     .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
+    //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
     .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was  With wrong username')
     .pause(configrationReader.getPauseValue())
     .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was  With wrong username')
@@ -77,14 +105,21 @@ exports.LoginWithWrongUsername = (browser) => {
 //Login with valid information
 exports.LoginWithValidInformation = (browser) => {
   browser
+  //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
+     //Check if email field is display
     .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
+    //Check if Password field is display
     .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
+    //Check if Sign in button is display or not
     .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     .pause(configrationReader.getPauseValue())
+    //Here to check that the login is successfully 
     .waitForElementVisible(loginSelectors.elements.UserMenu,configrationReader.getPeriod(),'Test was failed After you click on Sign in button because the login was not successfully even though the login was With Valid Information')
     .assert.containsText(loginSelectors.elements.UserMenu,configrationReader.getUserName(),'Test was failed After you click on Sign in button because the login was not successfully even though the login was With Valid Information') 
     
