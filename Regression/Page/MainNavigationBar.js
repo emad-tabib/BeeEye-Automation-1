4cbec13a105b10ae67c9bf76152862b9b6c0d0e6
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-02-25 15:36:59 +0200

    Update the assertion msg
    put "The assertion failed"  instead of "Test was failed"

--- Regression/Page/MainNavigationBar.js
+++ Regression/Page/MainNavigationBar.js
@@ -18,11 +18,11 @@ exports.MainNavigationBar = (browser) => {
     .waitForElementVisible('body', configrationReader.getPeriod()) // Wait till page loads
     //check the Logo if it is display
     .waitForElementVisible(MainNavigationBarSelectors.elements.Logo, 'Test is failed because the logo was not found in the Main Navigation Bar')
-    .assert.elementPresent(MainNavigationBarSelectors.elements.Logo, 'Test is failed because the logo was not found in the Main Navigation Bar')
+    .assert.elementPresent(MainNavigationBarSelectors.elements.Logo, 'The assertion failed because the logo was not found in the Main Navigation Bar')
 
     //Check if the Main Navigation Bar is display or not
     .waitForElementVisible(MainNavigationBarSelectors.elements.NavigationBar, 'Test is failed because the Navigation Bar was not found in the site')
-    .assert.elementPresent(MainNavigationBarSelectors.elements.NavigationBar, 'Test is failed because the Navigation Bar was not found in the site')
+    .assert.elementPresent(MainNavigationBarSelectors.elements.NavigationBar, 'The assertion failed because the Navigation Bar was not found in the site')
     //get pause
     .pause(configrationReader.getPauseValue());
 }
@@ -34,13 +34,13 @@ exports.DashboardNavigation = (browser) => {
     .waitForElementVisible('body', configrationReader.getPeriod()) // Wait till page loads
     //Check if Dashboard Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Dashboard, 'Test is failed because the Dashboard Link was not found in the Main Navigation Bar')
-    .assert.elementPresent(MainNavigationBarSelectors.elements.Dashboard, 'Test is failed because the Dashboard Link was not found in the Main Navigation Bar')
+    .assert.elementPresent(MainNavigationBarSelectors.elements.Dashboard, 'The assertion failed because the Dashboard Link was not found in the Main Navigation Bar')
     //Check what happen after you click on Dashboard link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Dashboard, function (result) {
       browser
         //Here to make sure that you are in Dashboard Page when you click on Dashboard Link by check if Small Card is display or not
         .waitForElementVisible(DashboardSelector.elements.SmallCard, 'Test is failed After you click on Dashboard Link because the link does not take you to Dashboard Page')
-        .assert.elementPresent(DashboardSelector.elements.SmallCard, 'Test is failed After you click on Dashboard Link because the link does not take you to Dashboard Page')
+        .assert.elementPresent(DashboardSelector.elements.SmallCard, 'The assertion failed After you click on Dashboard Link because the link does not take you to Dashboard Page')
     })
     //get pause
     .pause(configrationReader.getPauseValue());
@@ -53,13 +53,13 @@ exports.FlowsNavigation = (browser) => {
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if Flows Link is display in Main Navigation Bar
     .waitForElementVisible(FlowsSelector.elements.NavFlows, 'Test is failed because the Flows Link was not found in the Main Navigation Bar')
-    .assert.elementPresent(FlowsSelector.elements.NavFlows, 'Test is failed because the Flows Link was not found in the Main Navigation Bar')
+    .assert.elementPresent(FlowsSelector.elements.NavFlows, 'The assertion failed because the Flows Link was not found in the Main Navigation Bar')
     //Check what happen after you click on Flows link in the Navigation Bar
     .click(FlowsSelector.elements.NavFlows, function (result) {
       browser
         //Here to make sure that you are in Flows Page when you click on Flows Link by check if Flow Title is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.FlowTitle, 'Test is failed After you click on Flows Link because the link does not take you to Flows Page')
-        .assert.elementPresent(MainNavigationBarSelectors.elements.FlowTitle, 'Test is failed After you click on Flows Link because the link does not take you to Flows Page')
+        .assert.elementPresent(MainNavigationBarSelectors.elements.FlowTitle, 'The assertion failed After you click on Flows Link because the link does not take you to Flows Page')
     })
     //get pause
     .pause(configrationReader.getPauseValue());
@@ -72,13 +72,13 @@ exports.ProductionNavigation = (browser) => {
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if Production Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Production, 'Test is failed because the Production Link was not found in the Main Navigation Bar')
-    .assert.elementPresent(MainNavigationBarSelectors.elements.Production, 'Test is failed because the Production Link was not found in the Main Navigation Bar')
+    .assert.elementPresent(MainNavigationBarSelectors.elements.Production, 'The assertion failed because the Production Link was not found in the Main Navigation Bar')
     //Check what happen after you click on Production link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Production, function (result) {
       browser
         //Here to make sure that you are in Production Page when you click on Production Link by check if product count is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.ProductsCount, 'Test is failed After you click on Production Link because the link does not take you to Production Page')
-        .assert.elementPresent(MainNavigationBarSelectors.elements.ProductsCount, 'Test is failed After you click on Production Link because the link does not take you to Production Page')
+        .assert.elementPresent(MainNavigationBarSelectors.elements.ProductsCount, 'The assertion failed After you click on Production Link because the link does not take you to Production Page')
     })
     //get pause
     .pause(configrationReader.getPauseValue());
@@ -91,13 +91,13 @@ exports.ExplainNavigation = (browser) => {
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if Explain Link is display in Main Navigation Bar
     .waitForElementVisible(MainNavigationBarSelectors.elements.Explain, 'Test is failed because the Explain Link was not found in the Main Navigation Bar')
-    .assert.elementPresent(MainNavigationBarSelectors.elements.Explain, 'Test is failed because the Explain Link was not found in the Main Navigation Bar')
+    .assert.elementPresent(MainNavigationBarSelectors.elements.Explain, 'The assertion failed because the Explain Link was not found in the Main Navigation Bar')
     //Check what happen after you click on Explain link in the Navigation Bar
     .click(MainNavigationBarSelectors.elements.Explain, function (result) {
       browser
         //Here to make sure that you are in Explain Page when you click on Explain Link by check if Explain Wrapper is display or not
         .waitForElementVisible(MainNavigationBarSelectors.elements.ExplainWrapper, 'Test is failed After you click on Explain Link because the link does not take you to Explain Page')
-        .assert.elementPresent(MainNavigationBarSelectors.elements.ExplainWrapper, 'Test is failed After you click on Explain Link because the link does not take you to Explain Page')
+        .assert.elementPresent(MainNavigationBarSelectors.elements.ExplainWrapper, 'The assertion failed After you click on Explain Link because the link does not take you to Explain Page')
     })
     //get pause
     .pause(configrationReader.getPauseValue());
--- Regression/Page/login.js
+++ Regression/Page/login.js
@@ -15,19 +15,19 @@ exports.LoginWithoutPassword = (browser) => {
   //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if email field is display 
-    .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.email, 'The assertion failed because the email field was not found in the Login Page')
     //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
     //Check if Password field is display 
-    .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.password, 'The assertion failed because the password field was not found in the Login Page')
     //set the value for password field
     .setValue(loginSelectors.elements.password, '') // send values
     //Check if Sign in button is display or not
-    .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.signInBtn, 'The assertion failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     //Here to check that the login was not successfully and the user is still in Log in Page, also error msg is display
-    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was without password')
-    .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was without password')
+    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'The assertion failed After you click on Sign in button because the login was successfully even though the login was without password')
+    .assert.elementPresent(loginSelectors.elements.SignInError, 'The assertion failed After you click on Sign in button because no error msg displayed even though the login was without password')
     .pause(configrationReader.getPauseValue());
 }
 
@@ -38,19 +38,19 @@ exports.LoginWithoutUsername = (browser) => {
   //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if email field is display 
-    .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.email, 'The assertion failed because the email field was not found in the Login Page')
     //set the value for the email field
     .setValue(loginSelectors.elements.email, '') // send values
     //Check if Password field is display
-    .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.password, 'The assertion failed because the password field was not found in the Login Page')
     //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
     //Check if Sign in button is display or not
-    .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.signInBtn, 'The assertion failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
-    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was Without username')
-    .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was without username')
+    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'The assertion failed After you click on Sign in button because the login was successfully even though the login was Without username')
+    .assert.elementPresent(loginSelectors.elements.SignInError, 'The assertion failed After you click on Sign in button because no error msg displayed even though the login was without username')
     .pause(configrationReader.getPauseValue());
 
 }
@@ -61,19 +61,19 @@ exports.LoginWithWrongPassword = (browser) => {
   //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if email field is display
-    .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.email, 'The assertion failed because the email field was not found in the Login Page')
     //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
     //Check if Password field is display
-    .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.password, 'The assertion failed because the password field was not found in the Login Page')
     //set the value for password field
     .setValue(loginSelectors.elements.password, 'jj') // send values
     //Check if Sign in button is display or not
-    .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.signInBtn, 'The assertion failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
-    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was With wrong password')
-    .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was  With wrong password')
+    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'The assertion failed After you click on Sign in button because the login was successfully even though the login was With wrong password')
+    .assert.elementPresent(loginSelectors.elements.SignInError, 'The assertion failed After you click on Sign in button because no error msg displayed even though the login was  With wrong password')
     .pause(configrationReader.getPauseValue());
 
 }
@@ -83,20 +83,20 @@ exports.LoginWithWrongUsername = (browser) => {
   //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
     //Check if email field is display
-    .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.email, 'The assertion failed because the email field was not found in the Login Page')
     //set the value for the email field
     .setValue(loginSelectors.elements.email, 'user') // send values
     //Check if Password field is display
-    .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.password, 'The assertion failed because the password field was not found in the Login Page')
     //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
     //Check if Sign in button is display or not
-    .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.signInBtn, 'The assertion failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     //Here to check that the login was not successfully and the user is still in Log in Page , also error msg is display
-    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'Test was failed After you click on Sign in button because the login was successfully even though the login was  With wrong username')
+    .assert.elementPresent(loginSelectors.elements.SignInTitle, 'The assertion failed After you click on Sign in button because the login was successfully even though the login was  With wrong username')
     .pause(configrationReader.getPauseValue())
-    .assert.elementPresent(loginSelectors.elements.SignInError, 'Test was failed After you click on Sign in button because no error msg displayed even though the login was  With wrong username')
+    .assert.elementPresent(loginSelectors.elements.SignInError, 'The assertion failed After you click on Sign in button because no error msg displayed even though the login was  With wrong username')
     
 
 }
@@ -108,20 +108,20 @@ exports.LoginWithValidInformation = (browser) => {
   //wait the body to be loadded
     .waitForElementVisible('body', configrationReader.getPeriod()) // wait till page loads
      //Check if email field is display
-    .assert.elementPresent(loginSelectors.elements.email, 'Test was failed because the email field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.email, 'The assertion failed because the email field was not found in the Login Page')
     //set the value for the email field
     .setValue(loginSelectors.elements.email, configrationReader.getUserName()) // send values
     //Check if Password field is display
-    .assert.elementPresent(loginSelectors.elements.password, 'Test was failed because the password field was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.password, 'The assertion failed because the password field was not found in the Login Page')
     //set the value for password field
     .setValue(loginSelectors.elements.password, configrationReader.getPassword()) // send values
     //Check if Sign in button is display or not
-    .assert.elementPresent(loginSelectors.elements.signInBtn, 'Test was failed because the sign In Button was not found in the Login Page')
+    .assert.elementPresent(loginSelectors.elements.signInBtn, 'The assertion failed because the sign In Button was not found in the Login Page')
     .click(loginSelectors.elements.signInBtn)
     .pause(configrationReader.getPauseValue())
     //Here to check that the login is successfully 
     .waitForElementVisible(loginSelectors.elements.UserMenu,configrationReader.getPeriod(),'Test was failed After you click on Sign in button because the login was not successfully even though the login was With Valid Information')
-    .assert.containsText(loginSelectors.elements.UserMenu,configrationReader.getUserName(),'Test was failed After you click on Sign in button because the login was not successfully even though the login was With Valid Information') 
+    .assert.containsText(loginSelectors.elements.UserMenu,configrationReader.getUserName(),'The assertion failed After you click on Sign in button because the login was not successfully even though the login was With Valid Information') 
     
 
 }
