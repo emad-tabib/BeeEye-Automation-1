74b15351f6e30ba76a3eb82f4b532ceafbbe57b6
Mais Hasiba <55432493+MaisHasibaTestitg@users.noreply.github.com>
2020-03-24 09:54:15 +0200

    Flow Edit - DS : Columns  - search by feature name is working as expected
    Create new flow then back to flows Page to search on the created flow then access it to check if user access to last configration . After that , user upload CSV file then search by feature name in columns tab  to check if it was worked as expected

--- Regression/tests/FlowEditDSTests/FlowEditDSTests.js
+++ Regression/tests/FlowEditDSTests/FlowEditDSTests.js
@@ -43,97 +43,97 @@ describe('Dashboard Page : Test if these section are display or not: Best Flow(G
     afterEach(function (browser, done) {
         done();
     });
-    
-        //Verify user is navigated to the last confguring step for the selected flow
-        it('Verify user is navigated to the last confguring step for the selected flow', function (browser) {
-            setup.logTestDetails(this, "Try to Verify user is navigated to the last confguring step for the selected flow")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the last confguring step for the selected flow");
-            browser.end();
-        });
 
-        //Upload CSV file to not new flow
-        it('Verify Upload CSV file function is working as expected', function (browser) {
-            setup.logTestDetails(this, "Try to Verify Uploading CSV file function is working as expected")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Upload File for the selected Flow");
-            browser.end();
-        });
+    //Verify user is navigated to the last confguring step for the selected flow
+    it('Verify user is navigated to the last confguring step for the selected flow', function (browser) {
+        setup.logTestDetails(this, "Try to Verify user is navigated to the last confguring step for the selected flow")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the last confguring step for the selected flow");
+        browser.end();
+    });
 
-         //Select a new Label
-         it('Verify Selecting a new Label', function (browser) {
-            setup.logTestDetails(this, "Try to Verify Select a new Label")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            Flows.SelectValidLabel(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Select a new Label for the selected flow");
-            browser.end();
-        });
+    //Upload CSV file to not new flow
+    it('Verify Upload CSV file function is working as expected', function (browser) {
+        setup.logTestDetails(this, "Try to Verify Uploading CSV file function is working as expected")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Upload File for the selected Flow");
+        browser.end();
+    });
 
-        //Run the Data Source
-        it('Run the Data Source', function (browser) {
-            setup.logTestDetails(this, "Try to Run the Data Source")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            Flows.SelectValidLabel(browser);
-            Flows.RunDataSources(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Run the Data Source");
-            browser.end();
-        });
-        
-        //Verify no errors displayed in the log file
-        it('Verify no errors displayed in the log file', function (browser) {
-            setup.logTestDetails(this, "Try to Verify no errors displayed in the log file")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            Flows.SelectValidLabel(browser);
-            Flows.RunDataSources(browser);
-            Flows.VerifyTheLogFile(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the log file with no errors");
-            browser.end();
-        });
-        
-        //Select label with more than 2 values - show error message
-        it('Verify the error message was displayed when you select not valid Label', function (browser) {
-            setup.logTestDetails(this, " Try to verify the error message was displayed when you select not valid Label")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            Flows.SelectNotValidLabel(browser)
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you select not valid Label");
-            browser.end();
-        });
-        
-         //Verify Data Sources section is display as expected
-         it('Verify Data Sources section is display as expected', function (browser) {
-            setup.logTestDetails(this, " Try to verify the Data Sources section is display as expected After you Upload File for the Flow")
-            Flows.CreateNewFlow(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-            FlowEditDS.LastConfiguringStep(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-            Flows.UploadFile(browser);
-            Flows.VerifyDatasourceSection(browser);
-            screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for Data Sources section after you Upload File to the Selected Flow");
-            browser.end();
-        });
-        
+    //Select a new Label
+    it('Verify Selecting a new Label', function (browser) {
+        setup.logTestDetails(this, "Try to Verify Select a new Label")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.SelectValidLabel(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Select a new Label for the selected flow");
+        browser.end();
+    });
+
+    //Run the Data Source
+    it('Run the Data Source', function (browser) {
+        setup.logTestDetails(this, "Try to Run the Data Source")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.SelectValidLabel(browser);
+        Flows.RunDataSources(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you Run the Data Source");
+        browser.end();
+    });
+
+    //Verify no errors displayed in the log file
+    it('Verify no errors displayed in the log file', function (browser) {
+        setup.logTestDetails(this, "Try to Verify no errors displayed in the log file")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.SelectValidLabel(browser);
+        Flows.RunDataSources(browser);
+        Flows.VerifyTheLogFile(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the log file with no errors");
+        browser.end();
+    });
+
+    //Select label with more than 2 values - show error message
+    it('Verify the error message was displayed when you select not valid Label', function (browser) {
+        setup.logTestDetails(this, " Try to verify the error message was displayed when you select not valid Label")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.SelectNotValidLabel(browser)
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you select not valid Label");
+        browser.end();
+    });
+
+    //Verify Data Sources section is display as expected
+    it('Verify Data Sources section is display as expected', function (browser) {
+        setup.logTestDetails(this, " Try to verify the Data Sources section is display as expected After you Upload File for the Flow")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.VerifyDatasourceSection(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for Data Sources section after you Upload File to the Selected Flow");
+        browser.end();
+    });
+
 
     //Preview data - search by feature name is working as expected 
     it('Verify that search by feature name is working as expected ', function (browser) {
@@ -147,18 +147,31 @@ describe('Dashboard Page : Test if these section are display or not: Best Flow(G
         screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the result After you search by feature name in Preview Data section");
         browser.end();
     });
-    
-   //Check if Data are displayed in Columns tab
-   it('Verify if Data are displayed in Columns tab ', function (browser) {
-    setup.logTestDetails(this, " Try to verify if Data are displayed in Columns tab")
-    Flows.CreateNewFlow(browser);
-    screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
-    FlowEditDS.LastConfiguringStep(browser);
-    screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
-    Flows.UploadFile(browser);
-    Flows.CheckColumnsTab(browser);
-    screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the Data that are displayed in Columns tab");
-    browser.end();
-});
+
+    //Check if Data are displayed in Columns tab
+    it('Verify if Data are displayed in Columns tab ', function (browser) {
+        setup.logTestDetails(this, " Try to verify if Data are displayed in Columns tab")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.CheckColumnsTab(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the Data that are displayed in Columns tab");
+        browser.end();
+    });
+
+    //Check if search by Column is working as expected in columns tab
+    it('Verify that search by Column is working as expected in Columns tab ', function (browser) {
+        setup.logTestDetails(this, " Try to verify if search by Column is working as expected in Columns tab ")
+        Flows.CreateNewFlow(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for the new Flow");
+        FlowEditDS.LastConfiguringStep(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot for teh Last configuring step that user make it to the selected Flow");
+        Flows.UploadFile(browser);
+        Flows.SearchByFeatureNameInColumnTab(browser);
+        screenShotUtils.takeScreenShot(this, browser, "Here is the screenshot after you search by Column in Columns tab");
+        browser.end();
+    });
 
 });
